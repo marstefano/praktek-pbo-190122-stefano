16fc1a6d26eb561544f4603f239713a01a0739cd
marstefano <stefano.2020@smk-strada.sch.id>
2022-01-24 09:59:00 +0700

    tugas pbo

--- angkaGanjilGenap.js
+++ angkaGanjilGenap.js
@@ -2,7 +2,17 @@ for(let i = 1; i<=100; i+=3){
     if(i % 2 === 0){
        console.log(`${i} genap`);
     }
-    else{
-       console.log(`${i} ganjil`)
-    };
+    if(i%2!=0){
+       console.log(i+ "ganjil")
+    }
    } console.log('#####')
+
+   for (let i= 50; i <= 200;  i+=5) {
+      if (i % 3 == 0) {
+          console.log(i + "Bisa dibagi 3")
+      }
+  
+      if (i % 3 != 0) {
+          console.log(i + "Tidak bisa dibagi 3")
+      }
+  }
